12c0859032b591e10bb976e265fa0d24b5475233
gongmi <450989123@qq.com>
2017-08-16 09:14:41 +0800

    update proxy

--- comeon/src/DynaProxy/Test.java
+++ comeon/src/DynaProxy/Test.java
@@ -4,10 +4,13 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		// 创建一个原始的GunDog对象，作为target
 		Dog target = new GunDog();
+		MyInvocationHandler handler=new MyInvocationHandler(target);
 		// 以指定的target来创建动态代理
-		Dog dog = (Dog) MyProxyFactory.getProxy(target);
+		Dog dog = (Dog) handler.getProxy();
 		dog.info();
 		dog.run();
+		dog.equals(dog);
+		dog.toString();
 		System.out.println(dog.getClass());
 		System.out.println(target.getClass());
 	}
